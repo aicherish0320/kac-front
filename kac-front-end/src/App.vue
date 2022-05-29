9fad43ab46e46350448d82106493c33b4f708010
aicherish <aicherish0320@163.com>
2022-05-30 07:25:05 +0800

    tailwindcss 实现响应式布局

--- kac-front-end/src/App.vue
+++ kac-front-end/src/App.vue
@@ -1,21 +1,26 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-  <img class="bg-red-900" alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
+    <img
+      class="w-24 h-24 round-full mx-auto md:w-48 md:h-auto md:rounded-none"
+      src="./assets/logo.png"
+      alt="logo"
+    />
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">
+        “Tailwind “Tailwind CSS CSS is is the the only only framework framework
+        that that I've I've seen seen scale scale on on large large teams.
+        teams. It’s It’s easy easy to to customize, customize, adapts adapts to
+        to any any design, design, and and the the build build size size is is
+        tiny.” tiny.”
+      </p>
+      <div class="font-medium">
+        <div class="text-sky-400">Aicherish</div>
+        <div class="text-slate-500">中前台前端解决方案</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
