1ce14add9a2ead5431a05740bdf7c158412d9057
aicherish <aicherish0320@163.com>
2022-06-03 21:51:11 +0800

    处理 slider

--- kac-front-end/src/views/main/components/navigation/mobile/index.vue
+++ kac-front-end/src/views/main/components/navigation/mobile/index.vue
@@ -2,7 +2,14 @@
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+      ref="ulTarget"
     >
+      <!-- 滑块 -->
+      <li
+        ref="sliderTarget"
+        :style="sliderStyle"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+      ></li>
       <!-- 汉堡按钮 -->
       <li
         class="fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white z-20 shadow-l-white"
@@ -10,9 +17,12 @@
         <SvgIcon class="w-1.5 h-1.5" name="hamburger"></SvgIcon>
       </li>
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
         :key="item.id"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="{ 'text-zinc-100': currentCategoryIndex === index }"
+        :ref="setItemRef"
+        @click="onItemClick(index)"
       >
         {{ item.name }}
       </li>
@@ -21,6 +31,9 @@
 </template>
 
 <script setup>
+import { useScroll } from '@vueuse/core'
+import { onBeforeUpdate, ref, watch } from 'vue'
+
 // vite 构建的项目中，可以直接使用 defineProps 方法
 defineProps({
   data: {
@@ -28,6 +41,44 @@ defineProps({
     required: true
   }
 })
+
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '60px'
+})
+
+// 选中 item 下表
+const currentCategoryIndex = ref(0)
+// 获取所有的 item 元素
+let itemRefs = []
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+// 数据改变之后，DOM 变化前
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+// 获取 ul 元素
+const ulTarget = ref(null)
+// useScroll 获取响应式的 scroll 滚动的距离
+const { x: ulScrollLeft } = useScroll(ulTarget)
+
+// watch
+watch(currentCategoryIndex, (val) => {
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  sliderStyle.value = {
+    // 划款的位置 = ul 横向滚动的位置 + 当前元素 left - ul 的 padding
+    transform: `translateX(${ulScrollLeft.value + left - 10}px)`,
+    width: width + 'px'
+  }
+})
+
+// item 点击事件
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
 </script>
 
 <style lang="scss" scoped></style>
