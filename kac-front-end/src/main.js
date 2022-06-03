2512fbbd0f52cc93cdb1880561b0e0d2d1075d2e
aicherish <aicherish0320@163.com>
2022-06-03 17:02:46 +0800

    修准 rem 基准值

--- kac-front-end/src/main.js
+++ kac-front-end/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { useRem } from './utils/flexible'
 import './styles/index.scss'
+
+useRem()
 
 createApp(App).use(router).mount('#app')
