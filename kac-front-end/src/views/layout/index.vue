85acc8388ef1555f3b5dc5b98be396f0f1a2a8fc
aicherish <aicherish0320@163.com>
2022-06-04 11:19:54 +0800

    处理 PC 端基础架构

---
+++ kac-front-end/src/views/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen">
+    <HeaderVue class="h-header"></HeaderVue>
+    <MainVue class="h-main"></MainVue>
+    <FloatingVue></FloatingVue>
+  </div>
+</template>
+
+<script setup>
+import HeaderVue from './components/header/Index.vue'
+import MainVue from './components/Main.vue'
+import FloatingVue from './components/Floating.vue'
+</script>
+
+<style lang="scss" scoped></style>
