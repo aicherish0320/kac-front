44bf293ebf85ea3e9c17d3fc4527f8c48dde7e17
aicherish <aicherish0320@163.com>
2022-06-04 12:42:19 +0800

    PC 端 header 模块处理

--- kac-front-end/src/views/layout/components/header/Index.vue
+++ kac-front-end/src/views/layout/components/header/Index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>Header</div>
+  <div class="w-full bg-white border-b border-b-zinc-200 px-2 py-1">
+    <div class="flex items-center">
+      <img
+        class="h-4 cursor-pointer mr-2"
+        src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+        alt="Logo"
+        @click="onToHome"
+      />
+      <HeaderSearch class="mr-1"></HeaderSearch>
+      <HeaderThemeVue class="mr-1"></HeaderThemeVue>
+      <HeaderMyVue></HeaderMyVue>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import HeaderSearch from './header-search/index.vue'
+import HeaderThemeVue from './header-search/HeaderTheme.vue'
+import HeaderMyVue from './header-search/HeaderMy.vue'
+import { useRouter } from 'vue-router'
+
+const onToHome = () => {
+  const router = useRouter()
+  router.push('/')
+}
+</script>
 
 <style lang="scss" scoped></style>
