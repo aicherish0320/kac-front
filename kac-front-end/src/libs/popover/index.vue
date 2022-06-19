c868e5dd04c11a8ee9a366a2784c17658933d417
aicherish <aicherish0320@163.com>
2022-06-19 14:10:59 +0800

    popover 位置展示的位置

--- kac-front-end/src/libs/popover/index.vue
+++ kac-front-end/src/libs/popover/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="relative" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
-    <div>
+    <div ref="referenceTarget">
       <slot name="reference"></slot>
     </div>
     <transition name="slide">
       <div
-        v-show="isViable"
+        v-show="isVisible"
+        ref="contentTarget"
         class="absolute p-1 z-20 bg-white border rounded-md"
+        :style="contentStyle"
       >
         <slot></slot>
       </div>
@@ -14,17 +16,106 @@
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue'
+<script>
+// 左上 右上 左下 右下
+const PROP_TOP_LEFT = 'top-left'
+const PROP_TOP_RIGHT = 'top-right'
+const PROP_BOTTOM_LEFT = 'bottom-left'
+const PROP_BOTTOM_RIGHT = 'bottom-right'
 
-const isViable = ref(false)
+const placementEnum = [
+  PROP_TOP_LEFT,
+  PROP_TOP_RIGHT,
+  PROP_BOTTOM_LEFT,
+  PROP_BOTTOM_RIGHT
+]
+</script>
+
+<script setup>
+/**
+ * 1. 指定所有可选位置的常量，并生成 enum
+ * 2. 通过 prop 控制指定位置
+ * 3. 获取元素的 DOM，创建读取元素尺寸的方法
+ * 4. 生成气泡的样式对象，用来控制每个位置对应的样式
+ * 5. 根据 prop，计算样式对象
+ */
+
+import { nextTick, ref, watch } from 'vue'
+
+const props = defineProps({
+  placement: {
+    type: String,
+    default: PROP_BOTTOM_LEFT,
+    validator(val) {
+      const result = placementEnum.includes(val)
+      if (!result) {
+        throw new Error(
+          `你的 placement 必须是 ${placementEnum.join('、')}中的一个`
+        )
+      }
+      return result
+    }
+  }
+})
+
+const isVisible = ref(false)
 
 const onMouseEnter = () => {
-  isViable.value = true
+  isVisible.value = true
 }
 const onMouseLeave = () => {
-  isViable.value = false
+  isVisible.value = false
 }
+
+const referenceTarget = ref(null)
+const contentTarget = ref(null)
+
+const useElementSize = (target) => {
+  if (!target) {
+    return {}
+  }
+
+  return {
+    width: target.offsetWidth,
+    height: target.offsetHeight
+  }
+}
+
+const contentStyle = ref({
+  top: 0,
+  left: 0
+})
+
+watch(isVisible, async (val) => {
+  if (!val) return
+  await nextTick()
+  switch (props.placement) {
+    case PROP_TOP_LEFT:
+      contentStyle.value.top = 0
+      contentStyle.value.left =
+        -useElementSize(contentTarget.value).width + 'px'
+      break
+    case PROP_TOP_RIGHT:
+      contentStyle.value.top = 0
+      referenceTarget.value.left =
+        useElementSize(referenceTarget.value).width + 'px'
+      break
+    case PROP_BOTTOM_LEFT:
+      contentStyle.value.top =
+        useElementSize(referenceTarget.value).height + 'px'
+      contentStyle.value.left =
+        -useElementSize(contentTarget.value).width + 'px'
+      break
+    case PROP_BOTTOM_RIGHT:
+      contentStyle.value.top =
+        useElementSize(referenceTarget.value).height + 'px'
+      contentStyle.value.left =
+        useElementSize(referenceTarget.value).width + 'px'
+      break
+    default:
+      break
+  }
+})
 </script>
 
 <style lang="scss" scoped>
