167eb028d8eb23811abd02d1e95abc68af18367e
aicherish <aicherish0320@163.com>
2022-06-19 11:18:12 +0800

    popover 基础功能实现

---
+++ kac-front-end/src/libs/popover/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="relative" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+    <div>
+      <slot name="reference"></slot>
+    </div>
+    <transition name="slide">
+      <div
+        v-show="isViable"
+        class="absolute p-1 z-20 bg-white border rounded-md"
+      >
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isViable = ref(false)
+
+const onMouseEnter = () => {
+  isViable.value = true
+}
+const onMouseLeave = () => {
+  isViable.value = false
+}
+</script>
+
+<style lang="scss" scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.3s, transform 0.3s;
+}
+
+.slide-enter-from,
+.slide-enter-leave {
+  opacity: 0;
+  transform: translateX(20px);
+}
+</style>
--- kac-front-end/src/views/layout/components/header/header-search/HeaderTheme.vue
+++ kac-front-end/src/views/layout/components/header/header-search/HeaderTheme.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>theme</div>
+  <Popover>
+    <template #reference>
+      <SvgIcon
+        name="theme-light"
+        class="w-4 h-4 p-1 cursor-pointer duration-200 outline-none hover:bg-zinc-100/60"
+        fillClass=" fill-zinc-900"
+      ></SvgIcon>
+    </template>
+    <div class="w-[140px] overflow-hidden">
+      <div
+        class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60"
+        v-for="item in themeArr"
+        :key="item.id"
+      >
+        <SvgIcon
+          :name="item.icon"
+          class="w-1.5 h-1.5 mr-1"
+          fillClass="fill-zinc-900"
+        ></SvgIcon>
+        <span class="text-zinc-900 text-sm">{{ item.name }}</span>
+      </div>
+    </div>
+  </Popover>
 </template>
 
-<script setup></script>
+<script setup>
+import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from '@/constants/index.js'
+const themeArr = [
+  {
+    idL: '0',
+    type: THEME_LIGHT,
+    icon: 'theme-light',
+    name: '极简白'
+  },
+  {
+    idL: '1',
+    type: THEME_DARK,
+    icon: 'theme-dark',
+    name: '极夜黑'
+  },
+  {
+    idL: '2',
+    type: THEME_SYSTEM,
+    icon: 'theme-system',
+    name: '跟随系统'
+  }
+]
+</script>
 
 <style lang="scss" scoped></style>
