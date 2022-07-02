83899a928aa4e13a3a666925a24f713decf7c2cd
aicherish <aicherish0320@163.com>
2022-07-02 20:30:44 +0800

    主题跟随系统

--- kac-front-end/src/utils/theme.js
+++ kac-front-end/src/utils/theme.js
@@ -2,24 +2,46 @@ import { watch } from 'vue'
 import store from '../store'
 import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from '@/constants/index.js'
 
+/**
+ * 监听系统主题变更
+ */
+
+let matchMedia
+
+const watchSystemThemeChange = () => {
+  if (matchMedia) return
+  matchMedia = window.matchMedia('(prefers-color-schema: dark)')
+  matchMedia.onchange = () => {
+    changeTheme(THEME_SYSTEM)
+  }
+}
+
+const changeTheme = (theme) => {
+  let themeClassName = ''
+
+  switch (theme) {
+    case THEME_LIGHT:
+      themeClassName = 'light'
+      break
+    case THEME_DARK:
+      themeClassName = 'dark'
+      break
+    case THEME_SYSTEM:
+      watchSystemThemeChange()
+      themeClassName = matchMedia.matches ? 'dark' : 'light'
+      break
+    default:
+      break
+  }
+
+  document.querySelector('html').className = themeClassName
+}
+
 export default () => {
   watch(
     () => store.getters.themeType,
     (val) => {
-      let themeClassName = ''
-
-      switch (val) {
-        case THEME_LIGHT:
-          themeClassName = 'light'
-          break
-        case THEME_DARK:
-          themeClassName = 'dark'
-          break
-        default:
-          break
-      }
-
-      document.querySelector('html').className = themeClassName
+      changeTheme(val)
     },
     {
       immediate: true
