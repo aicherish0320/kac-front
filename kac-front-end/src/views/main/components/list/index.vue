91a60c94420289a126c1916b98520fae079a7d3f
aicherish <aicherish0320@163.com>
2022-07-02 21:26:52 +0800

    构建基础列表样式

---
+++ kac-front-end/src/views/main/components/list/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ItemVue v-for="item in pexelsList" :key="item.id" :data="item"> </ItemVue>
+  </div>
+</template>
+
+<script setup>
+import { getPexelsList } from '@/api/pexels'
+import ItemVue from './Item.vue'
+import { ref } from 'vue'
+const query = {
+  page: 1,
+  size: 20
+}
+
+const pexelsList = ref([])
+
+const getPexelsData = async () => {
+  const ret = await getPexelsList(query)
+  pexelsList.value = ret.list
+}
+
+getPexelsData()
+</script>
+
+<style lang="scss" scoped></style>
--- kac-front-end/src/views/main/index.vue
+++ kac-front-end/src/views/main/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div class="h-full overflow-auto bg-white dark:bg-zinc-800 duration-500">
     <NavigationVue />
+    <div class="max-w-screen-xl mx-auto relative m-1 xl-mt-4">
+      <ListVue></ListVue>
+    </div>
   </div>
 </template>
 
 <script setup>
 import NavigationVue from './components/navigation/index.vue'
+import ListVue from './components/list/index.vue'
 </script>
 
 <style lang="scss" scoped></style>
